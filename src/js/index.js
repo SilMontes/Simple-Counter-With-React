ad4edf9cea730299605e620ffed817e4258be905
SilMontes <Silmontes2001@gmail.com>
2021-03-06 17:38:04 +0000

    Terminado

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,31 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
 
 //render your react application
+function Home(props) {
+	return (
+		<div className="container">
+			<div>
+				<i className="fas fa-clock"></i>
+			</div>
+			<div className="sixthDigit">{props.sixthOne % 10}</div>
+			<div className="fifthDigit">{props.fifthOne % 10}</div>
+			<div className="fourthDigit">{props.fourthOne % 10}</div>
+			<div className="thirdDigit">{props.thirdOne % 10}</div>
+			<div className="secondDigit">{props.secondOne % 10}</div>
+			<div className="firstDigit">{props.firstOne % 10}</div>
+		</div>
+	);
+}
+Home.propTypes = {
+	sixthOne: PropTypes.number,
+	fifthOne: PropTypes.number,
+	fourthOne: PropTypes.number,
+	thirdOne: PropTypes.number,
+	secondOne: PropTypes.number,
+	firstOne: PropTypes.number
+};
 let counter = 0;
 setInterval(function() {
 	let sixth = Math.floor(counter / 100000);
@@ -20,7 +42,7 @@ setInterval(function() {
 	let third = Math.floor(counter / 100);
 	let second = Math.floor(counter / 10);
 	let first = Math.floor(counter / 1);
-	couter++;
+	counter++;
 
 	ReactDOM.render(
 		<Home
